363580bc6c0292abd4cea6f02e85a1c5e334cfe3
1337Ghostface <wu.portugal@gmail.com>
2026-07-31 18:21:27 +0100

    Fix comments for clarity in factorial calculation example

--- ex10.cpp
+++ ex10.cpp
@@ -12,12 +12,12 @@ int main() {
     cout << "Insira um número positivo: "; //usuario insere 5
     cin >> num;
     
-    soma = num; //soma = num. neste caso 5.
+    soma = num; //soma = num. neste caso 5
     
     cout << num << "!= "; //mostrar fatorial do numero. exemplo: 5!=
 
-    for (int i=num; i>1; i--){ //i = num (5), enquanto i for maior que 1 (maior que 1 porque queremos fazer o "for" 1 vez menos que o numero inicial), i menos 1.
-        soma = soma * (i-1); //soma (neste caso 5) = 5 x (i-1) como o i tem o valor de 5, vai ser 5 x (i-1 = 4) 4 x (i-1 = 3) 3 x (i-1 = 2) 2 x (i-1 = 1) 1.
+    for (int i=num; i>1; i--){ //i = num (5), enquanto i for maior que 1 (maior que 1 porque queremos fazer o "for" 1 vez menos que o numero inicial (5 passa a 4)), i menos 1.
+        soma = soma * (i-1); //soma (neste caso 5 porque já definimos isso na linha 15) = 5 x (i-1) como o i tem o valor de 5, vai ser 5 x (i-1 = 4) 4 x (i-1 = 3) 3 x (i-1 = 2) 2 x (i-1 = 1) 1.
         cout << i << " x "; //para mostrar a conta
     }
     
