8b5c2020f00715d12edd151cad9fa329fa8d9420
1337Ghostface <wu.portugal@gmail.com>
2026-07-31 18:24:11 +0100

    Refine comments for clarity in factorial calculation loop

--- ex10.cpp
+++ ex10.cpp
@@ -18,11 +18,10 @@ int main() {
 
     for (int i=num; i>1; i--){ //i = num (5), enquanto i for maior que 1 (maior que 1 porque queremos fazer o "for" 1 vez menos que o numero inicial (5 passa a 4)), i menos 1.
         soma = soma * (i-1); //soma (neste caso 5 porque já definimos isso na linha 15) = 5 x (i-1) como o i tem o valor de 5, vai ser 5 x (i-1 = 4) 4 x (i-1 = 3) 3 x (i-1 = 2) 2 x (i-1 = 1) 1.
-        cout << i << " x "; //para mostrar a conta (para antes de mostrar o 1 porque no for temos i>1. logo 1 nunca vai mostrar) - cont linha 24
+        cout << i << " x "; //para mostrar a conta (não mostra o 1 porque no "for" temos i>1. logo 1 nunca vai mostrar) - continuação linha 24
     }
     
-    cout << "1 = " << soma; //e o final da conta
-    // cout << "O fatorial de " << num << " é: " << soma << endl;
+    cout << "1 = " << soma; //e o final da conta incluindo o 1 que não podemos adicionar dentro do "for"
 
     return 0;
 }
